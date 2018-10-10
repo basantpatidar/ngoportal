275354d1e4b146ae06dcc73199eb6c0e0bf029b1
Basant Patidar <34193893+basantpatidar@users.noreply.github.com>
2018-10-09 23:37:42 -0400

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,8 @@
  * Date: 4/11/2018
  * Time: 2:47 PM
  */
-//config file
+//config file 
+// db connection and functions.php 
 require_once("db-settings.php"); //Require DB connection
 require_once("functions.php"); // Declare - Define all functions.
 date_default_timezone_set("America/New_York");
